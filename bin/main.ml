ba27e639881f57c0704b849a7998a64ea409c4c2
Pierre Villemot <tiky.halbaroth@gmail.com>
2022-10-10 15:28:02 +0200

    Get the home path from the environment.

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,9 @@
 open Benchtop
 
-let benchpress_share_dir = "/home/tiky/.local/share/benchpress"
+let benchpress_share_dir = 
+  let home_path = Unix.getenv "HOME" in
+  Filename.concat home_path ".local/share/benchpress"
+
 let configs_dir = List.hd Location.Sites.configs
 let tests_dir = List.hd Location.Sites.tests
 
