a2b8c7d18bf739c1621215f60fbe30fc34b2c3c2
Pierre Villemot <tiky.halbaroth@gmail.com>
2022-10-12 16:44:38 +0200

    Improve the debug message when benchpress failed

--- lib/misc.ml
+++ lib/misc.ml
@@ -4,6 +4,16 @@ let pp_list pp fmt lst =
   let pp_sep fmt () = Format.fprintf fmt ",@," in 
   Format.pp_print_list ~pp_sep pp fmt lst
 
+let pp_error_code fmt =
+  let open Unix in
+  function
+  | WEXITED rc -> 
+      Format.fprintf fmt "WEXITED %i" rc 
+  | WSIGNALED rc -> 
+      Format.fprintf fmt "WSIGNALED %i" rc 
+  | WSTOPPED rc -> 
+      Format.fprintf fmt "WSTOPPED %i" rc 
+
 let sprintf_list pp lst =
   let buf = Buffer.create 200 in
   let fmt = Format.formatter_of_buffer buf in
--- lib/misc.mli
+++ lib/misc.mli
@@ -1,5 +1,6 @@
 type 'a printer = Format.formatter -> 'a -> unit
 
-val sprintf_list : 'a printer -> 'a list -> string
 val pp_list : 'a printer -> 'a list printer
+val pp_error_code : Unix.process_status printer
+val sprintf_list : 'a printer -> 'a list -> string
 
