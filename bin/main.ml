45fa5b2e8870aefce1ee111b07650e1a1387a4c1
Pierrot <tiky.halbaroth@gmail.com>
2023-03-14 09:44:00 +0100

    Add an option to set the number of jobs. (#17)

--- bin/main.ml
+++ bin/main.ml
@@ -7,20 +7,20 @@ let header_logger inner_handler request =
     Format.fprintf fmt "%s: %s" key value
   ) in
   Dream.debug (fun log -> log "Http Header:@,%a" pp_fields fields);
-  let _ = 
+  let _ =
     match Dream.header request "Content-type" with
     | Some content_type -> begin
         match String.split_on_char ';' content_type with
         | "application/x-www-form-urlencoded"::_ -> begin
           Dream.form request >|= function
           | `Ok form_fields ->
-              Dream.debug 
+              Dream.debug
                 (fun log -> log "POST fields:@,%a" pp_fields form_fields)
           | _ -> ()
-          end 
+          end
         | _ -> Lwt.return ()
         end
-    | None -> Lwt.return () 
+    | None -> Lwt.return ()
   in
   inner_handler request
 
@@ -29,12 +29,13 @@ let rec update_context () =
   let* _ = Context.update () in
   update_context ()
 
-let server log_level interface port = 
+let server log_level interface port number_of_jobs =
   let log_level = Option.value log_level ~default:`Info in
   let interface = Option.value interface ~default:"localhost" in
   let port = Option.value port ~default:8080 in
+  let j = Option.value number_of_jobs ~default:Options.number_of_jobs in
   Dream.initialize_log ~level:log_level ();
-  Dream.(serve ~interface ~port 
+  Dream.(serve ~interface ~port
   @@ logger
   @@ memory_sessions
   @@ header_logger
@@ -45,13 +46,13 @@ let server log_level interface port =
     ; get "/" @@ Handlers.handle_rounds_list
     ; scope "/round" [origin_referrer_check] [
         get "/:uuid" @@ Handlers.handle_round_detail
-      ; get "/:uuid/problem/:problem" 
+      ; get "/:uuid/problem/:problem"
         @@ Handlers.handle_problem_trace
       ; get "/:uuid1/diff/:uuid2" @@ Handlers.handle_rounds_diff
       ; post "/action" @@ Handlers.handle_round_action_dispatcher
       ]
     ; scope "/benchpress" [origin_referrer_check] [
-        post "/schedule" @@ Handlers.handle_schedule_round
+        post "/schedule" @@ Handlers.handle_schedule_round ~j
       ; post "/stop" @@ Handlers.handle_stop_round
     ]
     ; scope "/problems" [origin_referrer_check] [
@@ -59,11 +60,11 @@ let server log_level interface port =
     ]
   ])
 
-let main log_level interface port =
+let main log_level interface port number_of_jobs =
   Lwt_main.run (
     let* _ = Context.init () in
     Lwt.async (update_context);
-    server log_level interface port)
+    server log_level interface port number_of_jobs)
 
 module Cmd = struct
   open Cmdliner
@@ -74,20 +75,25 @@ module Cmd = struct
         "info", `Info
       ; "warning", `Warning
       ; "error", `Error
-      ; "debug", `Debug 
+      ; "debug", `Debug
     ] in
-    Arg.(value & opt (some level) ~vopt:(Some `Info) None 
-      & info ["l"; "log"] ~docv:"LOG_LEVEL" ~doc) 
+    Arg.(value & opt (some level) ~vopt:(Some `Info) None
+      & info ["l"; "log"] ~docv:"LOG_LEVEL" ~doc)
 
   let interface =
     let doc = "Specify the listen address." in
-    Arg.(value & opt (some string) None 
+    Arg.(value & opt (some string) None
       & info ["i"; "interface"] ~docv:"INTERFACE" ~doc)
 
   let port =
     let doc = "Specify the listen port." in
-    Arg.(value & opt (some int) None 
+    Arg.(value & opt (some int) None
       & info ["p"; "port"] ~docv:"PORT" ~doc)
+
+  let number_of_jobs =
+    let doc = "Set the number of jobs." in
+    Arg.(value & opt (some int) None
+      & info ["j"] ~docv:"NUMBER" ~doc)
 
   let cmd =
     let open Cmdliner in
@@ -98,10 +104,11 @@ module Cmd = struct
       ; `S Manpage.s_bugs; `P "Bug reports to <pierre.villemot@ocamlpro.com>"
     ] in
     let info = Cmd.info "benchtop" ~version:"dev" ~doc ~man in
-    Cmd.v info Term.(const main $ log_level $ interface $ port)
+    Cmd.v info Term.(const main $ log_level $ interface $ port
+      $ number_of_jobs)
 
   let main () = exit (Cmd.eval cmd)
 end
 
-let () = Cmd.main () 
-  
+let () = Cmd.main ()
+
